ecec38ed59a1a0e837b0bf8d047012f822edc5a2
zzvvmm <zzvvmm96@gmail.com>
2018-07-06 18:05:29 +0700

    Chapter 3: Mostly Static Pages

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,8 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    I18n.available_locales = [:en, :vi]
+    I18n.default_locale = :en
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
