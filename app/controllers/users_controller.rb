e4d7c833514c1783d5fc26ae9e5e25ab9a53571f
zzvvmm <zzvvmm96@gmail.com>
2018-07-09 16:47:42 +0700

    Chapter 7: Sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,29 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t("flash_success")
+      redirect_to user_path @user
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+
+    return if @user
+    flash[:fail] = t("flash_fail")
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
 end
